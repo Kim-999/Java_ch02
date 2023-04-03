fd7cfd56f4ced23403c8dca64b4ec667ed82b7a1
JBEDU29 <JBEDU29@DESKTOP-HMF1SKG>
2023-04-03 16:08:44 +0900

    자바 ch02 변수 선언 예제 추가

---
+++ src/test01/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * 소스파일: Hello.java
+ */
+
+package test01;
+
+public class Hello {
+	public static int sum(int n,int m) {
+		return n+m;
+	}
+
+	public static void main(String[] args) {
+		int i=20;
+		int s;
+		char a;
+		
+		s=sum(i,10);
+		a='?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+		// TODO Auto-generated method stub
+
+	}
+
+}
